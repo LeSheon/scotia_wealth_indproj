91184fc12883349b4348829954181fe172adcae2
Marwan KN <marwannofal@gmail.com>
2023-10-06 11:53:25 -0400

    Complete Header component

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,7 +1,23 @@
 import React from "react";
-
+import "./Header.scss";
+import closeIcon from "../../assets/icons/X.svg";
+import moreIcon from "../../assets/icons/3dot_button.svg";
 const Header = () => {
-  return <header></header>;
+  return (
+    <header className="header">
+      <div className="header__container-top">
+        <h2 className="header__title-main">Scotia Smart Investor</h2>
+        <img src={closeIcon} className="header__icon-close" />
+      </div>
+      <div className="header__container-bottom">
+        <div>
+          <h6 className="header__title-second">Scotia</h6>
+          <h4 className="header__title-third">Smart Investor</h4>
+        </div>
+        <img src={moreIcon} className="header__icon-more" />
+      </div>
+    </header>
+  );
 };
 
 export default Header;
--- src/pages/DashBoard/DashBoard.js
+++ src/pages/DashBoard/DashBoard.js
@@ -1,12 +1,5 @@
 import "./dashboard.scss";
-import scotiaIcon from "../../assets/icons/scotiabank-4.svg";
-import NavBar from "../../components/NavBar/NavBar";
-import Header from "../../components/Header/Header";
 
 export default function DashBoard() {
-  return (
-    <main className="dashboard">
-      <Header />
-    </main>
-  );
+  return <></>;
 }
